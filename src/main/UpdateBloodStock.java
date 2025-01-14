1ffd6a8507bfaf4f087e9d85d7d3fe1514deb378
Ishtiaque hossain <istiaquehossainpranto@gmail.com>
2025-01-14 18:05:25 +0600

    Added new ui for admin & super admin

--- src/main/UpdateBloodStock.java
+++ src/main/UpdateBloodStock.java
@@ -1,10 +1,10 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
-public class userAppointPage extends JFrame implements ActionListener
+public class UpdateBloodStock extends JFrame implements ActionListener
 {
 
-    public userAppointPage()
+    public UpdateBloodStock()
     {
         UserUI();
     }
@@ -20,7 +20,7 @@ public class userAppointPage extends JFrame implements ActionListener
         panel.add(createUpperpanel());
         panel.add(createLowerpanel());
         
-        ImageIcon appIcon = new ImageIcon("HealthCareCenter/src/main/resources/Icons/appIcon.png");
+        ImageIcon appIcon = new ImageIcon("resources/Icons/appIcon.png");
         this.setIconImage(appIcon.getImage());
 
         this.setTitle("Health Care Center");      
@@ -41,7 +41,7 @@ public class userAppointPage extends JFrame implements ActionListener
         JPanel upper_panel = new JPanel();                                  
         upper_panel.setLayout(null);
         upper_panel.setBounds(0,0,900,200);
-        upper_panel.setBackground(Color.white);
+        upper_panel.setBackground(Color.gray);
 
         JLabel label = new JLabel("Health Care Center");
         label.setHorizontalAlignment(JLabel.CENTER);
@@ -79,74 +79,74 @@ public class userAppointPage extends JFrame implements ActionListener
         JPanel middle_panel = new JPanel();                                  
         middle_panel.setLayout(null);
         middle_panel.setBounds(0,150,900,50);
-        middle_panel.setBackground(new Color(25, 25, 105));
+        middle_panel.setBackground(new Color(10, 10, 10));
 
 
         //level for home
         JLabel home = new JLabel();
         home.setText("Home");
-        home.setForeground(Color.red);
+        home.setForeground(new Color(0x00FF00));
         home.setFont(new Font("SansSerif", Font.PLAIN, 15));
-        home.setBounds(25, 15, 100, 20);
+        home.setBounds(20, 15, 60, 20);
 
 
 
-         //level for appoinment
-         JLabel appoinment= new JLabel();
-         appoinment.setText("Book Appoinment");
-         appoinment.setForeground(new Color(200, 120, 150));
-         appoinment.setFont(new Font("SansSerif", Font.PLAIN, 15));
-         appoinment.setBounds(110, 15, 165, 20);
+         //level for Manage Doctors
+         JLabel Mang_Doc= new JLabel();
+         Mang_Doc.setText("Manage Doctors");
+         Mang_Doc.setForeground(new Color(0x00FF00));
+         Mang_Doc.setFont(new Font("SansSerif", Font.PLAIN, 15));
+         Mang_Doc.setBounds(80, 15, 140, 20);
 
-          //level for history
-        JLabel History = new JLabel();
-        History.setText("View Medicle History");
-        History.setForeground(new Color(200, 120, 150));
-        History.setFont(new Font("SansSerif", Font.PLAIN, 15));
-        History.setBounds(275, 15, 200, 20);
+          //level forupd_bloo
+         JLabel upd_bloo = new JLabel();
+         upd_bloo.setText("Update Blood Stock");
+         upd_bloo.setForeground(Color.RED );
+         upd_bloo.setFont(new Font("SansSerif", Font.PLAIN, 15));
+         upd_bloo.setBounds(200, 15, 160, 20);
+		 
+		 //level for manage admin
+         JLabel Mang_Admin = new JLabel();
+         Mang_Admin.setText("Manage Admin");
+         Mang_Admin.setForeground(new Color(0x00FF00));
+         Mang_Admin.setFont(new Font("SansSerif", Font.PLAIN, 15));
+         Mang_Admin.setBounds(380, 15, 120, 20);
 
-         //level for blood
-         JLabel blood = new JLabel();
-         blood.setText("Blood Bank");
-         blood.setForeground(new Color(200, 120, 150));
-         blood.setFont(new Font("SansSerif", Font.PLAIN, 15));
-         blood.setBounds(475, 15, 100, 20);
-
-          //level for bill
-        JLabel bill = new JLabel();
-        bill.setText("Pay Bill");
-        bill.setForeground(new Color(200, 120, 150));
-        bill.setFont(new Font("SansSerif", Font.PLAIN, 15));
-        bill.setBounds(625, 15, 100, 20);
+         //level for oth_sys_manag 
+         JLabel oth_sys_manag = new JLabel();
+         oth_sys_manag .setText("Other System Management");
+         oth_sys_manag .setForeground(new Color(0x00FF00));
+         oth_sys_manag .setFont(new Font("SansSerif", Font.PLAIN, 15));
+         oth_sys_manag .setBounds(510, 15, 225, 20);
 
          //level for log out
          JLabel log_out = new JLabel();
-         log_out.setText("log_out");
-         log_out.setForeground(new Color(200, 120, 150));
+         log_out.setText("Log out");
+         log_out.setForeground(new Color(0x00FF00));
          log_out.setFont(new Font("SansSerif", Font.PLAIN, 15));
          log_out.setBounds(750, 15, 250, 20);
  
           //add level in middle_panel
           middle_panel.add(home);
-          middle_panel.add(appoinment);
-          middle_panel.add(History);
-          middle_panel.add(blood);
-          middle_panel.add(bill);
+          middle_panel.add(Mang_Doc);
+          middle_panel.add(upd_bloo);
+          middle_panel.add(oth_sys_manag );
+		  middle_panel.add(Mang_Admin);
           middle_panel.add(log_out);
 
 
         home.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-               home.setForeground(Color.red);
-               home.setBounds(23, 10, 110, 30);
-               home.setFont(new Font("SansSerif", Font.PLAIN, 17));
+               home.setForeground(new Color(0x00FF00));
+               home.setBounds(15, 10, 70, 30);
+               home.setFont(new Font("SansSerif", Font.PLAIN, 18));
     
             }
             @Override
           public void mouseExited(MouseEvent e) {
-                home.setForeground(Color.red); 
-                home.setBounds(25, 15, 100, 20);
+                home.setForeground(new Color(200, 120, 150)); 
+                home.setBounds(20, 15, 60, 20);
                 home.setFont(new Font("SansSerif", Font.PLAIN, 15));
                  
             }
@@ -158,104 +158,111 @@ public class userAppointPage extends JFrame implements ActionListener
 
 
 
-        appoinment.addMouseListener(new MouseAdapter() {
+        Mang_Doc.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                appoinment.setForeground(new Color(0x00FF00));
-                appoinment.setBounds(100, 10, 205, 30);
-                appoinment.setFont(new Font("SansSerif", Font.PLAIN, 17));
+                Mang_Doc.setForeground(new Color(0x00FF00));
+                Mang_Doc.setFont(new Font("SansSerif", Font.PLAIN, 18));
+				Mang_Doc.setBounds(70, 10, 150, 30);
             }
             @Override
             public void mouseExited(MouseEvent e) {
-                appoinment.setForeground(new Color(200, 120, 150));
-                appoinment.setBounds(110, 15, 165, 20);
-                appoinment.setFont(new Font("SansSerif", Font.PLAIN, 15));
+                Mang_Doc.setForeground(new Color(200, 120, 150));
+                Mang_Doc.setFont(new Font("SansSerif", Font.PLAIN, 15));
+				Mang_Doc.setBounds(80, 15, 140, 20);
             }
             @Override
             public void mouseClicked(MouseEvent e) {
-                SwingUtilities.getWindowAncestor(appoinment).dispose();
+                SwingUtilities.getWindowAncestor(home).dispose();
                 
             }
         });
 
-        History.addMouseListener(new MouseAdapter() {
+       upd_bloo.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                History.setForeground(new Color(0x00FF00));
-                History.setBounds(265,5, 220, 40);
-                History.setFont(new Font("SansSerif", Font.PLAIN,17));
+               upd_bloo.setForeground(Color.RED );
+               upd_bloo.setFont(new Font("SansSerif", Font.PLAIN, 18));
+			   upd_bloo.setBounds(210, 10, 170, 30);
+			  
             }
             @Override
             public void mouseExited(MouseEvent e) {
-                History.setForeground(new Color(200, 120, 150));
-                History.setBounds(275, 15, 200, 20);
-                History.setFont(new Font("SansSerif", Font.PLAIN, 15));
+               upd_bloo.setForeground(Color.RED );
+               upd_bloo.setFont(new Font("SansSerif", Font.PLAIN, 15));
+			   upd_bloo.setBounds(220, 15, 160, 20);
+			  
             }
             @Override
             public void mouseClicked(MouseEvent e) {
-                SwingUtilities.getWindowAncestor(History).dispose();  
+                
+            }
+        });
+		
+		Mang_Admin.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+               Mang_Admin.setForeground(new Color(0x00FF00));
+               Mang_Admin.setFont(new Font("SansSerif", Font.PLAIN, 18));
+			   Mang_Admin.setBounds(370, 10, 130, 30);
+			  
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+               Mang_Admin.setForeground(new Color(200, 120, 150));
+               Mang_Admin.setFont(new Font("SansSerif", Font.PLAIN, 15));
+			   Mang_Admin.setBounds(380, 15, 120, 20);
+			  
+            }
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                
             }
         });
         
 
 
-        blood .addMouseListener(new MouseAdapter() {
+        oth_sys_manag  .addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                blood .setForeground(new Color(0x00FF00));
-                blood.setBounds(470, 10, 120, 30);
-                blood.setFont(new Font("SansSerif", Font.PLAIN, 17));
+                oth_sys_manag .setForeground(new Color(0x00FF00));
+                oth_sys_manag .setFont(new Font("SansSerif", Font.PLAIN, 18));
+				oth_sys_manag .setBounds(520, 10, 230, 30);
+				
             }
             @Override
             public void mouseExited(MouseEvent e) {
-                blood .setForeground(new Color(200, 120, 150));
-                blood.setBounds(475, 15, 100, 20);
-                blood.setFont(new Font("SansSerif", Font.PLAIN, 15));
+                oth_sys_manag .setForeground(new Color(200, 120, 150));
+                oth_sys_manag .setFont(new Font("SansSerif", Font.PLAIN, 15));
+				oth_sys_manag .setBounds(510, 15, 225, 20);
+				
             }
             @Override
             public void mouseClicked(MouseEvent e) {
-                SwingUtilities.getWindowAncestor(blood).dispose(); 
+                
             }
         });
 
-
-
-        bill.addMouseListener(new MouseAdapter() {
-          @Override
-          public void mouseEntered(MouseEvent e) {
-            bill.setForeground(new Color(0x00FF00));
-            bill.setBounds(620, 5, 120, 40);
-            bill.setFont(new Font("SansSerif", Font.PLAIN, 17));
-          }
-          @Override
-          public void mouseExited(MouseEvent e) {
-            bill.setForeground(new Color(200, 120, 150));
-            bill.setBounds(625, 15, 100, 20);
-            bill.setFont(new Font("SansSerif", Font.PLAIN, 15));
-          }
-          @Override
-          public void mouseClicked(MouseEvent e) {
-            SwingUtilities.getWindowAncestor(bill).dispose();  
-          }
-      });
 
 
       log_out.addMouseListener(new MouseAdapter() {
           @Override
           public void mouseEntered(MouseEvent e) {
             log_out.setForeground(new Color(0x00FF00));
-            log_out.setBounds(747, 10, 280, 30);
-            log_out.setFont(new Font("SansSerif", Font.PLAIN, 17));
+            log_out.setFont(new Font("SansSerif", Font.PLAIN, 18));
+			log_out.setBounds(740, 10, 200, 30);
+			
           }
           @Override
         public void mouseExited(MouseEvent e) {
             log_out.setForeground(new Color(200, 120, 150));
-            log_out.setBounds(750, 15, 250, 20);
             log_out.setFont(new Font("SansSerif", Font.PLAIN, 15));
+			log_out.setBounds(750, 15, 250, 20);
+			
           }
           @Override
           public void mouseClicked(MouseEvent e) {
-            SwingUtilities.getWindowAncestor(log_out).dispose();
+              
           }
       });
 
@@ -269,12 +276,6 @@ public class userAppointPage extends JFrame implements ActionListener
         lower_panel.setLayout(null);
         lower_panel.setBounds(0,200,900,500);
         lower_panel.setBackground(Color.white);
-        
-        /*ImagePanel image_panel = new ImagePanel("HealthCareCenter/src/main/resources/Images/signUpBG1.jpg");
-        image_panel.setBounds(0,0,900,300);
-        image_panel.setLayout(null);
-        image_panel.setOpaque(false);
-        lower_panel.add(image_panel);*/
 
         JLabel welcome= new JLabel("Welcome User");
         welcome.setHorizontalAlignment(JLabel.CENTER);
@@ -299,9 +300,8 @@ public class userAppointPage extends JFrame implements ActionListener
         // code to handle the action event
     }
 
-    
     public static void main(String[] args) {
-        new userAppointPage().setVisible(true);
+        new UpdateBloodStock();
     }
 
 }
